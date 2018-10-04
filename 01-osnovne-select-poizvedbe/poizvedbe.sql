48923c2bd01ff502bd02738a58bde2b16f14416e
Matija Pretnar <matija@pretnar.info>
2018-10-04 16:34:42 +0200

    Poizvedbe s prvih predavanj

--- 01-osnovne-select-poizvedbe/poizvedbe.sql
+++ 01-osnovne-select-poizvedbe/poizvedbe.sql
@@ -15,15 +15,21 @@ SELECT naslov,
        dolzina / 60
   FROM
 
+-- Če želimo natančen rezultat, moramo dolžino pretvoriti
+-- iz celega števila, saj sicer dobimo celoštevilsko deljenje
+SELECT naslov,
+       CAST (dolzina AS FLOAT) / 60
+  FROM
+
 -- Če želimo, lahko podatke tudi poimenujemo
 SELECT naslov,
-       dolzina / 60 AS dolzina_v_urah
+       CAST (dolzina AS FLOAT) / 60 AS dolzina_v_urah
   FROM filmi;
 
 -- Če damo ime v narekovaje ali [...], lahko vsebuje tudi presledke,
 -- vendar to ni priporočeno, ker je zamudno.
 SELECT naslov,
-       dolzina / 60 AS [dolzina v urah]
+       CAST (dolzina AS FLOAT) / 60 AS [dolzina v urah]
   FROM filmi;
 
 -- Iskane podatke lahko omejimo z WHERE.
@@ -290,6 +296,40 @@ SELECT reziser, leto, naslov, certifikat
        )
   ORDER BY reziser, leto;
 
+-- Spodnja poizvedba vrne vse filme režiserjev, ki so posneli
+-- tako družinski kot družinam neprijazen film.
+
+SELECT *
+  FROM filmi
+ WHERE reziser IN (
+           SELECT DISTINCT reziser
+             FROM filmi
+            WHERE certifikat = 'G'
+       )
+AND 
+       reziser IN (
+           SELECT DISTINCT reziser
+             FROM filmi
+            WHERE certifikat = 'R'
+       )
+ ORDER BY reziser,
+          leto;
+
+-- Ta poizvedba ne vrne nič rezultatov, saj bi iskala režiserje
+-- tistih filmov, ki imajo hkrati certifikata G in R. Takih seveda
+-- ni, saj ima vsak film natanko en certifikat.
+
+SELECT *
+  FROM filmi
+ WHERE reziser IN (
+           SELECT DISTINCT reziser
+             FROM filmi
+            WHERE certifikat = 'G'
+              AND certifikat = 'R'
+       )
+ ORDER BY reziser,
+          leto;
+
 -- Poizvedbe počasi postajajo bolj zapletene. Kako se znajdemo?
 -- Začnemo jih pisati po kosih.
 
@@ -326,3 +366,138 @@ SELECT * FROM filmi WHERE
 AND
   ocena NOT BETWEEN 5.5 AND 8.5;
 ORDER BY leto;
+
+-- najvišja ocena kateregakoli filma
+SELECT MAX(ocena) 
+  FROM filmi;
+
+-- najkrajša dolžina kateregakoli filma
+SELECT MIN(dolzina) 
+  FROM filmi;
+
+-- najkrajša, povprečna in največja dolžina kateregakoli filma
+SELECT MIN(dolzina),
+       AVG(dolzina),
+       MAX(dolzina) 
+  FROM filmi;
+
+-- največja dolžina filma, ki ga je režiral Steven Spielberg
+SELECT max(dolzina) 
+  FROM filmi
+ WHERE reziser = 'Steven Spielberg';
+
+-- povprečna dolžina filmov, ki jih je režiral Steven Spielberg
+SELECT avg(ocena) 
+  FROM filmi
+ WHERE reziser = 'Steven Spielberg';
+
+-- vsi filmi, ki jih je režiral Charlie Chaplin
+SELECT *
+  FROM filmi
+ WHERE reziser = 'Charles Chaplin';
+
+-- skupna dolžina vseh filmov, ki jih je režiral Charlie Chaplin
+SELECT sum(dolzina) 
+  FROM filmi
+ WHERE reziser = 'Charles Chaplin';
+
+-- število filmov, ki jih je režiral Charlie Chaplin, pri katerih imamo podatke
+-- o dolžini/oceni/certifikatu/režiserju
+-- Običajno so vse številke enake, razen če kje kakšen podatek manjka
+SELECT count(dolzina),
+       count(ocena),
+       count(certifikat),
+       count(reziser) 
+  FROM filmi
+ WHERE reziser = 'Charles Chaplin';
+
+-- Če želimo prešteti ujemajočevrstice, uporabimo count( * )
+SELECT count( * ) 
+  FROM filmi
+ WHERE reziser = 'Charles Chaplin';
+
+-- Dolžina najkrajšega Spielbergovega filma je 107 minut. Toda kateri film je to?
+SELECT MIN(dolzina) 
+  FROM filmi
+ WHERE reziser = 'Steven Spielberg';
+
+-- Včasih dela tudi spodnja poizvedba, vendar se raje NE ZANAŠAJTE NA TO
+SELECT naslov, MIN(dolzina) 
+  FROM filmi
+ WHERE reziser = 'Steven Spielberg';
+
+-- Lahko pogledamo, da je najkrajši Spielbergov film dolg 107 minut in nato
+-- napišemo ustrezno poizvedbo
+SELECT *
+  FROM filmi
+ WHERE dolzina = 107 AND 
+       reziser = 'Steven Spielberg';
+
+-- Še bolje pa je uporabiti gnezdeni SELECT.
+-- Ta nam vrne vse filme Stevena Spielberga, pri katerih je
+-- dosežena minimalna dolžina.
+SELECT *
+  FROM filmi
+ WHERE dolzina = (
+                     SELECT MIN(dolzina) 
+                       FROM filmi
+                      WHERE reziser = 'Steven Spielberg'
+                 )
+AND 
+       reziser = 'Steven Spielberg';
+
+-- Ta pa nam vrne vse filme, ki so tako dolgi kot najkrajši
+-- film Stevena Spielberga.
+SELECT *
+  FROM filmi
+ WHERE dolzina = (
+                     SELECT min(dolzina) 
+                       FROM filmi
+                      WHERE reziser = 'Steven Spielberg'
+                 );
+
+-- Če nas zanima samo en ekstrem, je bolj enostavno, da
+-- uporabimo ORDER BY in LIMIT 1.
+SELECT *
+  FROM filmi
+ WHERE reziser = 'Steven Spielberg'
+ ORDER BY dolzina
+ LIMIT 1;
+
+-- Povprečna ocena vseh filmov
+SELECT avg(ocena) 
+  FROM filmi;
+
+-- Podatki vseh filmov hkrati z absolutno razliko do povprečne vrednosti ocene
+SELECT *,
+       ABS(ocena - 6.94) 
+  FROM filmi;
+
+-- Podatki vseh filmov hkrati s tem, ali se ocena od povprečja razlikuje za manj kot 0.1
+SELECT *
+  FROM filmi
+ WHERE ABS(ocena - 6.94) < 0.1;
+
+-- Vsi povprečni filmi, torej tisti, ki se od povprečja razlikujejo za manj kot 0.1
+SELECT *
+  FROM filmi
+ WHERE ocena BETWEEN 6.94 - 0.1 AND 6.94 + 0.1;
+
+-- Boljša rešitev: gnezdeni SELECT
+SELECT *
+  FROM filmi
+ WHERE ABS(ocena - (
+                       SELECT avg(ocena) 
+                         FROM filmi
+                   )
+       ) < 0.1;
+
+-- Vsi filmi, ki so bili slabši od najslabšega Shyamalanovega filma
+SELECT *
+  FROM filmi
+ WHERE ocena < (
+                   SELECT min(ocena) 
+                     FROM filmi
+                    WHERE reziser LIKE '%Shyamalan'
+               );
+
