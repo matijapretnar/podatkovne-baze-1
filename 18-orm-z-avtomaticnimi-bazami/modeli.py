4f574e73bf74dc25894b9ea271631ba5659c3da6
Matija Pretnar <matija@pretnar.info>
2019-03-07 10:45:08 +0100

    Teste izloči iz modelov

--- 18-orm-z-avtomaticnimi-bazami/modeli.py
+++ 18-orm-z-avtomaticnimi-bazami/modeli.py
@@ -1,12 +1,6 @@
-import baza
 import sqlite3
-import os
-
-os.chdir('/Users/matija/Documents/Matija/podatkovne-baze-1-2/18-orm-z-avtomaticnimi-bazami/')
 
 conn = sqlite3.connect('filmi.db')
-baza.ustvari_bazo_ce_ne_obstaja(conn)
-conn.execute('PRAGMA foreign_keys = ON')
 
 class ObicajnoPolje:
     TEXT = 'TEXT'
@@ -221,43 +215,3 @@ class Oseba(Zapis):
 
     def __str__(self):
         return self.ime
-
-
-##################################################################
-
-filmi_2010 = Film.poisci_vse(leto=2010, ocena=6.7)
-assert next(filmi_2010).naslov == 'Ljubezen in druge droge'
-assert next(filmi_2010).naslov == 'Robin Hood'
-assert next(filmi_2010).leto == 2010
-
-avatar = Film.poisci_enega(naslov='Avatar')
-assert avatar.naslov == 'Avatar'
-assert avatar.ocena == 7.8
-
-avatar.ocena = 7.9
-avatar.shrani()
-assert len(list(Film.poisci_vse(naslov='Avatar'))) == 1
-avatar.ocena = 7.8
-avatar.shrani()
-assert len(list(Film.poisci_vse(naslov='Avatar'))) == 1
-
-assert {zanr.naziv for zanr in avatar.zanri()} == {'Fantasy', 'Adventure', 'Action'}
-
-
-ime_predmeta = 'Podatkovne baze 1'
-podatkovne_baze_1 = Film(naslov=ime_predmeta, dolzina=2000, leto=2018, zasluzek=0)
-podatkovne_baze_1.shrani()
-assert len(list(Film.poisci_vse(naslov=ime_predmeta))) == 1
-podatkovne_baze_1.pobrisi()
-assert len(list(Film.poisci_vse(naslov=ime_predmeta))) == 0
-
-for zanr in avatar.zanri():
-    print(zanr)
-    for _, film in zip(range(10), zanr.filmi()):
-        print('-', film)
-
-lepi_leonardo = Oseba.poisci_enega(ime='Leonardo DiCaprio')
-for film in lepi_leonardo.filmi():
-    print(film)
-    for soigralec_lepega_leonarda in film.osebe():
-        print('-', soigralec_lepega_leonarda)
